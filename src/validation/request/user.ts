bd937e0a22013c59e9ee7c9e7c32844aaaa2a308
libstake <polling.choi@hotmail.com>
2022-05-10 18:06:28 +0900

    Implement user validation scheme

--- src/validation/request/user.ts
+++ src/validation/request/user.ts
@@ -1,6 +1,23 @@
 import Schema, { Type, boolean, string } from 'computed-types';
 import { EmailRegex, NationalPhoneNumber } from '../regex';
 
+const GetUserInfoQuerySchema = Schema.either(
+    Schema({}, { strict: true }), // As User
+    Schema({    // As Admin
+        via: string.equals('classifier'),
+        target_id: string,
+    }, { strict: true }),
+);
+export type GetUserInfoParam = Type<typeof GetUserInfoQuerySchema>;
+export const GetUserInfoParamValidator = GetUserInfoQuerySchema.destruct();
+
+const UpdateUserInfoBodySchema = Schema({
+    realname: string.min(1).strictOptional(),
+    mobile: string.regexp(NationalPhoneNumber).strictOptional(),
+}, { strict: true });
+export type UpdateUserInfoBody = Type<typeof UpdateUserInfoBodySchema>;
+export const UpdateUserInfoBodyValidator = UpdateUserInfoBodySchema.destruct();
+
 const SignupBodySchema = Schema({
     email: EmailRegex,
     username: string.min(2),
@@ -11,6 +28,8 @@ const SignupBodySchema = Schema({
 export type SignupBody = Type<typeof SignupBodySchema>;
 export const SignupBodyValidator = SignupBodySchema.destruct();
 
+/* No Sign-out Schema */
+
 const LoginBodySchema = Schema({
         email: EmailRegex,
         password: string.min(1),
@@ -20,15 +39,18 @@ const LoginBodySchema = Schema({
 export type LoginBody = Type<typeof LoginBodySchema>;
 export const LoginBodyValidator = LoginBodySchema.destruct();
 
-const LinkTermBodySchema = Schema({
-        allow_access_personal_information: boolean.equals(true).strictOptional(),
-}, { strict: true });
-export type LinkTermBody = Type<typeof LinkTermBodySchema>;
-export const LinkTermBodyValidator = LinkTermBodySchema.destruct();
+const GetTokenInfoQuerySchema = Schema.either(
+    Schema({}, { strict: true }),   // As User
+    Schema({    // As Admin
+        via: string.equals('classifier'),
+        target_id: string,
+    }, { strict: true })
+);
+export type GetTokenInfoQuerySchema = Type<typeof GetTokenInfoQuerySchema>;
+export const GetTokenInfoQueryValidator = GetTokenInfoQuerySchema.destruct();
 
 const UnlinkBodySchema = Schema.either(
-    Schema({}, { strict: true }), // As User
-    Schema({    // As Admin
+    Schema({    // As User & Admin
         via: string.equals('classifier'),
         target_id: string,
     }, { strict: true }),
@@ -36,12 +58,11 @@ const UnlinkBodySchema = Schema.either(
 export type UnlinkBody = Type<typeof UnlinkBodySchema>;
 export const UnlinkBodyValidator = UnlinkBodySchema.destruct();
 
-const GetUserInfoParamSchema = Schema.either(
-    Schema({}, { strict: true }), // As User
-    Schema({    // As Admin
-        via: string.equals('classifier'),
-        target_id: string,
-    }, { strict: true }),
-);
-export type GetUserInfoParam = Type<typeof GetUserInfoParamSchema>;
-export const GetUserInfoParamValidator = GetUserInfoParamSchema.destruct();
+/* No Log-out Schema */
+
+// TODO: TBD
+// const LinkTermBodySchema = Schema({
+//     allow_access_personal_information: boolean.equals(true).strictOptional(),
+// }, { strict: true });
+// export type LinkTermBody = Type<typeof LinkTermBodySchema>;
+// export const LinkTermBodyValidator = LinkTermBodySchema.destruct();
